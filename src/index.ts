2c0c0d7132135cef79d669694ecfb7333fafebfc
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-04-23 05:23:56 -0700

    fix: oops remove stuff used for testing for writeup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,1 @@
 export * from "./cl"
-
-declare const TS_TYPE_ERROR: unique symbol
-type TS_TYPE_ERROR<Message extends string> = { [TS_TYPE_ERROR]: Message }
-
-type ValidateAStar<S extends string> =
-  S extends '' ? true
-  : S extends `a${infer T}` ? ValidateAStar<T>
-  : false
-
-type AStarConstraint<S> =
-	[S] extends [string]
-		? ValidateAStar<S> extends true
-      ? string
-      : TS_TYPE_ERROR<'S does not satisfy AStar!'>
-    : TS_TYPE_ERROR<'S must be a string!'>
-			 
-
-type T<S extends AStarConstraint<S>> = S
